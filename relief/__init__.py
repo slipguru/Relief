c44720beed02d3257f71846f07785fb3c748f9c8
Matteo Barbieri <matteo.barbieri@dibris.unige.it>
2017-02-08 16:00:33 +0100

    Relief family variable selection algorithm - first commit

---
+++ relief/__init__.py
@@ -0,0 +1,15 @@
+"""Relief package.
+
+Package containing the implementation if the IRelief variable selection
+algorithm
+
+References
+----------
+
+[1]
+
+"""
+
+__version__ = 0.1
+
+from .irelief import IRelief
